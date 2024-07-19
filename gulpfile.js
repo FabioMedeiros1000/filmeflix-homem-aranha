4706d61c65c56204c654034d06d9513d0d27fcca
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-07-19 19:29:23 -0300

    automatizando tarefas: compressão e compilação

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const changed = require('gulp-changed');
+const uglify = require('gulp-uglify');
+const replace = require('gulp-replace');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
 
 function compilaSass(){
     return gulp.src('./src/styles/*.scss')
@@ -16,7 +20,43 @@ function comprimeImagens(){
         .pipe(gulp.dest('./dist/images'));
 }
 
-// exports.default = comprimeImagens;
+function comprimeJavascript(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'));
+}
+
+// function moveHtml(){
+//     return gulp.src('./src/index.html')
+//         .pipe(rename({ suffix: '-copy' }))
+//         .pipe(gulp.dest('./temp'))
+//         .pipe(gulp.src('./temp/*.html'))
+//         .pipe(replace('ENDERECO_CSS', './styles/main.css'))
+//         .pipe(replace('ENDERECO_JAVASCRIPT', './scripts/main.js'))
+//         .pipe(gulp.dest('./dist/'))
+//         .pipe(gulp.src('./temp', { read: false, allowEmpty: true }))
+//         .pipe(clean({ force: true }));
+// }
+
+function moveHtml() {
+    return gulp.src('./src/index.html') // Seleciona o arquivo HTML original
+        .pipe(rename({ suffix: '-copy' })) // Renomeia o arquivo com um sufixo
+        .pipe(gulp.dest('./temp')) // Salva a cópia na pasta temporária
+        .on('end', function() {
+            gulp.src('./temp/index-copy.html') // Seleciona o arquivo renomeado
+                .pipe(replace('ENDERECO_CSS', './styles/main.css')) // Substitui o endereço do CSS
+                .pipe(replace('ENDERECO_JAVASCRIPT', './scripts/main.js')) // Substitui o endereço do JS
+                .pipe(rename({ basename: 'index' })) // Renomeia para 'index.html'
+                .pipe(gulp.dest('./dist/')) // Salva os arquivos modificados na pasta final
+                .on('end', function() {
+                    // Após a substituição ser concluída, exclui a pasta temporária
+                    gulp.src('./temp', { read: false, allowEmpty: true })
+                        .pipe(clean({ force: true }));
+                });
+        });
+}
+
+exports.dist = gulp.parallel(comprimeImagens, compilaSass, comprimeJavascript, moveHtml);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(compilaSass, comprimeImagens))
 }
